b3c8f17eb1a24461c5fabf0738f5684a6a376775
Sophie <c2595690@tees.ac.uk>
2025-12-09 15:29:51 +0000

    hopefully resolved no functions in inventory service func after deployment bug

---
+++ inventory-service-func/src/app.ts
@@ -0,0 +1,16 @@
+// Main entry point for the Inventory Service Azure Functions App
+// This file imports all function modules so their registrations run.
+
+import { app } from "@azure/functions";
+
+// HTTP trigger functions
+import "./functions/adjust-inventory-stock-http";
+import "./functions/get-inventory-http";
+import "./functions/getInventoryByDeviceHttp";
+import "./functions/list-inventory-http";
+
+// Event-driven functions
+import "./functions/updateInventoryFromLoanEvent";
+
+export { app };
+export default app;
